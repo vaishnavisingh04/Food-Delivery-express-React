92efbc59fc723fd52ba29ab5e6d8bfebf4abbca0
Vaishnavi Singh <vaishnavisingh4546@gmail.com>
2024-12-06 12:37:56 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// {}--> it takes html tags atrributes like ID, CLASSES
+//react.createElement(object)-->Html(browser understands it )
+// basically render conversts object into html tags then give to the browser .
+//if we want create nested html like
+
+
+/* <div id="parent">
+       <div id="child">
+           <h1>hello world</h1>
+           <h2> I am h2 tag</h2>
+        </div>
+        <div id="child2">
+           <h1>hello world</h1>
+           <h2> I am h2 tag</h2>
+        </div>
+
+     </div>
+*/
+
+
+
+
+
+const heading= 
+React.createElement("div" ,{id:"parent"}, 
+  [React.createElement("div" ,{id:"child"},
+     [React.createElement("h1" ,{}, "I am H1 tag"),React.createElement("h2" ,{}, "I am H2 tag")]
+), React.createElement("div" ,{id:"child2"},
+ [   React.createElement("h1" ,{}, "I am H1 tag"),
+  React.createElement("h2" ,{}, "I am H2 tag")
+  ]), 
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
