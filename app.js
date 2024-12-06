b35e6191edd4597059c619b86c9087796fc399ba
Vaishnavi Singh <vaishnavisingh4546@gmail.com>
2024-12-06 13:23:54 +0530

    Initial commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ React.createElement("div" ,{id:"parent"},
   [React.createElement("div" ,{id:"child"},
      [React.createElement("h1" ,{}, "I am H1 tag"),React.createElement("h2" ,{}, "I am H2 tag")]
 ), React.createElement("div" ,{id:"child2"},
- [   React.createElement("h1" ,{}, "I am H1 tag"),
+ [   gitReact.createElement("h1" ,{}, "I am H1 tag"),
   React.createElement("h2" ,{}, "I am H2 tag")
   ]), 
 ]);
